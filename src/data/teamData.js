8405a7b808f345db0b77a047db35525d2aeab695
smymchennai-cloud <smymchennai@gmail.com>
2026-02-01 15:34:23 +0530

    [Atul] Adding more photos

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -4,10 +4,10 @@ export const officeBearers = [
   { name: 'Kushal Maheswari', position: 'Secretary', photo: '/photos/kushal.png', photoPosition: 'top' },
   { name: 'Hemanth Bhattad', position: 'Vice President', photo: '/photos/hemanth.png' },
   { name: 'Ketan Bisani', position: 'Vice President', photo: '/photos/ketan.png' },
-  { name: 'Mohit Bajaj', position: 'Treasurer', photo: '/photos/mohit.png' },
+  { name: 'Mohit Bajaj', position: 'Treasurer', photo: '/photos/mohit.png', photoPosition: 'center 20%' },
   { name: 'Shubham Sarda', position: 'Joint Secretary', photo: '/photos/shubham.png' },
   { name: 'Nikhil Mohta', position: 'Joint Secretary', photo: '/photos/nikhil.png' },
-  { name: 'Madan Mohan Rathi', position: 'IPP', photo: '/photos/madan.png' }
+  { name: 'Madan Mohan Rathi', position: 'IPP', photo: '/photos/madan.png', photoPosition: 'center 20%' }
 ];
 
 // Executive Committee Members
@@ -16,7 +16,7 @@ export const executiveMembers = [
   { name: 'Ankit Rathi', position: 'Executive Member', photo: '/photos/ankit.png' },
   { name: 'Arnav Maheshwari', position: 'Executive Member', photo: '/photos/arnav.png' },
   { name: 'Arvind Kumar Jetha', position: 'Executive Member', photo: '/photos/arvind.png' },
-  { name: 'Ashish Maheswari', position: 'Executive Member', photo: '/photos/ashish.png' },
+  { name: 'Ashish Maheswari', position: 'Executive Member', photo: '/photos/ashish.png', photoPosition: 'center 20%' },
   { name: 'Atul Maheswari', position: 'Executive Member', photo: '/photos/atul.png', photoPosition: 'center 20%' },
   { name: 'Chirag Jhawar', position: 'Executive Member', photo: '/photos/chirag.png', photoPosition: 'center 20%' },
   { name: 'Girish Singh Mohta', position: 'Executive Member', photo: '/photos/girish.png', photoPosition: 'center 20%' },
@@ -35,7 +35,7 @@ export const executiveMembers = [
 // Advisors
 export const advisors = [
   { name: 'Anil Kumar Kela', position: 'Advisor', photo: '/photos/anil.png' },
-  { name: 'Jai Prakash Malpani', position: 'Advisor', photo: '/photos/jaiprakash.png' },
+  { name: 'Jai Prakash Malpani', position: 'Advisor', photo: '/photos/jaiprakash.png', photoPosition: 'center 20%' },
   { name: 'Praful Mohta', position: 'Advisor', photo: '/photos/praful.png' },
   { name: 'Pramod Sarda', position: 'Advisor', photo: '/photos/pramod.png' },
   { name: 'Vinod Dwarkani', position: 'Advisor', photo: '/photos/vinod.png' }
